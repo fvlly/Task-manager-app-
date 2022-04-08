a9349d5efec317085d2d6c73c0963216636ece68
Fvlly <luki0000001@gmail.com>
2022-04-08 22:11:29 +0100

    light mode with borders

--- public/index.js
+++ public/index.js
@@ -1,7 +1,22 @@
 const menuIconButton = document.querySelector("[data-menu-icon-btn");
 const sideNav = document.querySelector("[data-sidenav]");
+const switchBox = document.querySelector("[data-switch-box]");
+const toggler = document.querySelector("[data-toggle-switch]")
 
 menuIconButton.addEventListener("click", () => {
   console.log("clicked");
   sideNav.classList.toggle("open");
+
+
 });
+
+switchBox.addEventListener("click", ()=>{
+  if (sideNav.classList.contains(open) && toggler.checked ) {
+    switchBox.innerHTML = "Dark mode"
+  } else {
+    switchBox.innerHTML = "light mode"
+  }
+})
+
+
+
