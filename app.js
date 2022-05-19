d2e04c92ed3fe6695078333fca465d54e9ae0001
Fvlly <luki0000001@gmail.com>
2022-05-20 00:25:59 +0100

    removed local env virables

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require("express");
+
 require("./db/mongoose");
-const ejs = require("ejs");
-require("dotenv").config();
 
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static("public"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+
 
 // // middleware for maintenance
 // app.use((req, res, next) => {
